a663ecdecdbf3c2c591d7b62d6e553fdb7b4afb1
Guy Balaam <guy.balaam@unity3d.com>
2022-08-31 13:54:19 +0100

    Adding uGUI

--- Assets/TouchDebugger.cs
+++ Assets/TouchDebugger.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.UI;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 
 public class TouchDebugger : MonoBehaviour
 {
     [SerializeField] private Text text;
     [SerializeField] public int touchFrames;
+    [SerializeField] public int enhancedTouchFrames;
+    [SerializeField] public int buttonPresses;
 
     public void SetFrameRate(int fps)
     {
@@ -30,6 +33,11 @@ public class TouchDebugger : MonoBehaviour
         }
     }
 
+    public void ButtonPress()
+    {
+        buttonPresses++;
+    }
+
     private void Start()
     {
         SetFrameRate(2);
@@ -44,8 +52,12 @@ public class TouchDebugger : MonoBehaviour
 
     private void UpdateInput()
     {
-        if (Touchscreen.current == null) return;
+        foreach (var touch in Touch.activeTouches)
+        {
+            if (touch.isInProgress) enhancedTouchFrames++;
+        }
         
+        if (Touchscreen.current == null) return;
         foreach (var touch in Touchscreen.current.touches)
         {
             var phase = touch.phase.ReadValue();
@@ -58,6 +70,9 @@ public class TouchDebugger : MonoBehaviour
     
     private void UpdateCanvas()
     {
-        text.text = $"Touch Frames: {touchFrames}\n";
+        text.text =
+            $"Touch Frames: {touchFrames}\n" +
+            $"Enhanced Touch Frames: {enhancedTouchFrames}\n" +
+            $"Button Presses: {buttonPresses}";
     }
 }
